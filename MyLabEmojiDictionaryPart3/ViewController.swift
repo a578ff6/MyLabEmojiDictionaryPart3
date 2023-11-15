cb413b3a84381f5cf5cafb800b8a1995ada6453a
wayne tsao <a578ff6@gmail.com>
2023-11-16 04:14:24 +0800

    Initial Commit

---
+++ MyLabEmojiDictionaryPart3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLabEmojiDictionaryPart3
+//
+//  Created by 曹家瑋 on 2023/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
